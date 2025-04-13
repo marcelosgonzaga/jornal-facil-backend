b07458d142af7760ff4e1e060dd6a92be92e601d
marcelosgonzaga <marcelosgonzaga@gmail.com>
2025-04-13 16:10:49 -0300

    Initial commit - Estrutura básica do projeto

---
+++ src/main/java/redeinova/jornalfacil/JornalFacilApplication.java
@@ -0,0 +1,13 @@
+package redeinova.jornalfacil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JornalFacilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JornalFacilApplication.class, args);
+    }
+
+}
---
+++ src/test/java/redeinova/jornalfacil/JornalFacilApplicationTests.java
@@ -0,0 +1,13 @@
+package redeinova.jornalfacil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JornalFacilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
